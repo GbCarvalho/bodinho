8dc94ae70e1feea38debb4a59474833b3ef02f3d
Carvalho <braga.gabrielcarvalho@gmail.com>
2022-04-26 23:21:06 -0300

    (+) Adding socket.io to server api

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,3 +1,15 @@
+import { Server } from "socket.io";
 export default defineEventHandler(({ req, res }) => {
+  if (res.socket.server.io) {
+    console.log("Socket already initialized");
+  } else {
+    console.log("Socket is initializing");
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
+
+    res.socket.server.io.on("connection", (socket) => {
+    });
+  }
+
   res.end();
 });
